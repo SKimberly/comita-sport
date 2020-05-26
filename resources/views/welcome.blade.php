73aa83fb31bbc07cdf7ed700bb7729a0287260a5
SKimberly <kimi.uatf@gmail.com>
2020-05-26 02:28:22 -0400

    Primera configuracion, login y register hecho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ComitaSport
                 </div>
 
                 <div class="links">
