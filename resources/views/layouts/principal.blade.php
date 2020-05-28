4f9bad506b8a03a4a98f89ea49fbce559dc64b3e
SKimberly <kimi.uatf@gmail.com>
2020-05-28 02:31:46 -0400

    Continuando diseño pagina principal, imagenes, colores, lo q faltaba, hasta footer

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -52,7 +52,7 @@
                     </ul>
                 </div>
             </nav>
-            <section class="caratula">
+            <section class="caratula" id="inicio">
                 <div class="contenedor">
                     <div class="caratula-cont row">
                         @yield('saludo')
@@ -64,22 +64,178 @@
             </section>
             <div class="ola" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-2.25,74.50 C256.20,250.16 247.74,-88.31 502.25,74.50 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
         </div>
-        <!--<div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <main>
+            <section class="about-services" id="servicio">
+                <div class="contenedor">
+                    <h2 class="titulo">SERVICIO</h2>
+                    <div class="servicio-cont">
+                        <div class="servicio-ind">
+                            <img src="{{ asset('/img/welcome/dudas.svg') }}" alt="">
+                            <h3>Atendemos tus dudas</h3>
+                            <p>Atendemos rápidamente cualquier consulta que tengas via chat. No estás sólo, sino que siempre estamos atentos a tus inquietudes.</p>
+                        </div>
+
+                        <div class="servicio-ind">
+                            <img src="{{ asset('/img/welcome/money.svg') }}" alt="">
+                            <h3>Pago seguro</h3>
+                            <p>Todo pedido que realices será confirmado a travéz de una llamada. Todos los pagos los puedes realizar contra entrega el valor acordado.</p>
+                        </div>
+                        <div class="servicio-ind">
+                            <img src="{{ asset('/img/welcome/security.svg') }}" alt="">
+                            <h3>Información privada</h3>
+                            <p>Los pedidos que realices sólo los conocerás tú a través de tu panel de usuario. Nadie más tiene acceso a esta información.</p>
+                        </div>
+                    </div>
                 </div>
-            @endif
-            <div class="content">
+            </section>
+            <section class="producto" id="productos">
+                <div class="contenedor">
+                    <h2 class="titulo">CATEGORIA DE LOS PRODUCTOS</h2>
+                    <div class="galeria-port">
+                    <!--@if(isset($categorias))
+                        @foreach($categorias as $categoria)
+                            <div class="imagen-port">
+                                <img src="{{ $categoria->urlcate }}" alt="Categoria Foto">
+                                <div class="hover-galeria">
+                                    <a href="{{ route('categoria.productos', $categoria->id) }}" class="text-center" style="text-decoration: none;">
+                                    <img src="{{ asset('/img/icono1.png') }}" alt="">
+                                    <p>{{ $categoria->nombre }}</p>
+                                    <p class="text-justify" style="padding-left:12px; padding-right: 12px;">{{ $categoria->descripcion }}</p>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif-->
+                    </div>
+                </div>
+            </section>
+            <section class="clientes-sin-clase" id="opiniones">
+                <div class="contenedor">
+                <h2 class="titulo">Qué opinan nuestro clientes</h2>
+                <div class="cards">
+                    <div class="card-opinion">
+                        <img src="{{ asset('/img/welcome/mujer.jpg') }}" alt="">
+                        <div class="contenido-textos-card">
+                            <h4>Maria Ortuño Miranda</h4>
+                            <p>Buena atención... y muy puntuales en la entrega de su trabajo, gracias sigan adelante!</p>
+                        </div>
+                    </div>
+                    <div class="card-opinion">
+                        <img src="{{ asset('/img/welcome/hombre.jpeg') }}" alt="">
+                        <div class="contenido-textos-card">
+                            <h4>Juan Claros Fuerte</h4>
+                            <p>Gracias por su excelente servicio, buena calidad en sus productos.</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section class="sobre-nosotros" id="nosotros">
+                <div class="contenedor">
+                <h2 class="titulo">
+                    INFORMACIÓN
+                </h2>
+                <div class="contenedor-sobre-nosotros row">
+                    <img src="{{ asset('/img/welcome/portada.svg') }}" alt="img-sobre-nosotros" class="imagen-developer">
+                    <div class="contenido-textos col-md-6">
+                        <h3 class="titulo-nosotros"><span>1</span> ¿Quiénes somos?</h3>
+                        <p>Sport La Comita, somos una empresa lider en el mercado de implemento y artículos deportivos, que buscamos posicionamiento en el estado Boliviano, brindando productos de calidad.</p>
+
+                        <h3 class="titulo-nosotros"><span>2</span> ¿Qué ofrecemos?</h3>
+                        <p>La fábrica de ropa "Sport La Comita" ofrece a su distinguida clientela todo tipo de ropa deportiva, como ser: Poleras, cortos, polerones, chaquetas, busos, parkas, chamarras, etc. Con los diseños a gusto del cliente de la mejor calidad.</p>
+
+                        <h3 class="titulo-nosotros"><span>3</span> ¿Por qué esta página web?</h3>
+                        <p>Te ofrecemos la facilidad de poder ver nuestros productos, realizar cotizaciones, consultar sobre los ultimos o nuevos diseños, todo eso en está página web.</p>
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section class="about-services" id="contacto" >
+                <div class="contenedor">
+                    <h2 class="titulo">CONTÁCTANOS</h2>
+                    <div class="servicio-cont col-lg-12 d-flex align-items-stretch">
+                        <div class="col-12 col-lg-6" >
+                            <form class="bg-white shadow rounded py-3 px-4 was-validated"
+                                method="POST"
+                                action="/">
+                                 @csrf
+                                 <div class="form-group">
+                                    <label for="nombre">Nombre:</label>
+                                    <input class="form-control bg-light shadow-sm {{ $errors->has('nombre') ? ' is-invalid' : 'border-0' }}"
+                                        id="nombre"
+                                        name="nombre"
+                                        placeholder="Ingrese su nombre completo" value="{{ old('nombre') }}" required >
+                                    @if ($errors->has('nombre'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('nombre') }}</strong>
+                                        </span>
+                                    @endif
+                                 </div>
+                                 <div class="form-group">
+                                    <label for="telefono">Celular/Teléfono:</label>
+                                    <input class="form-control bg-light shadow-sm {{ $errors->has('telefono') ? ' is-invalid' : 'border-0' }}"
+                                        id="telefono"
+                                        name="telefono"
+                                        placeholder="Ingrese su teléfono o celular." value="{{ old('telefono') }}" type="number" required >
+                                    @if ($errors->has('telefono'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('telefono') }}</strong>
+                                        </span>
+                                    @endif
+                                 </div>
+                                <div class="form-group">
+                                    <label for="email">E-mail:</label>
+                                    <input class="form-control bg-light shadow-sm {{ $errors->has('email') ? ' is-invalid' : 'border-0' }}" id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="Ingrese su correo eléctronico" value="{{ old('email') }}" required >
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group">
+                                    <label for="contenido">Contenido:</label>
+                                    <textarea class="form-control bg-light shadow-sm {{ $errors->has('contenido') ? ' is-invalid' : 'border-0' }}" rows="4" name="contenido" placeholder="Ingrese el contenido de su mensaje" required>{{ old('contenido') }}</textarea>
+                                    @if ($errors->has('contenido'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('contenido') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button type="submit" class="btn btn-comita text-white btn-lg btn-block">Enviar</button>
+                            </form>
+                        </div><hr>
+                        <div class="col-12 col-lg-6 shadow embed-responsive embed-responsive-1by1">
+                          <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d939.7436407343811!2d-65.75643947083583!3d-19.585585699174853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93f94e76c52e1119%3A0xed6b59af8ea1b01f!2sOruro%20178%2C%20Villa%20Imperial%20de%20Potos%C3%AD!5e0!3m2!1ses-419!2sbo!4v1570242707496!5m2!1ses-419!2sbo"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <div class="contenedor-footer">
+                <div class="content-foo">
+                    <h4>Celular</h4>
+                    <p>70462939</p>
+                </div>
+                <div class="content-foo">
+                    <h4>E-mail</h4>
+                    <p>sport.lacomita19@gmail.com</p>
+                </div>
+                <div class="content-foo">
+                    <h4>Dirección (Tienda)</h4>
+                    <p>Calle Oruro Nro. 184</p>
+                </div>
+                <div class="content-foo">
+                    <h4>Dirección (Taller)</h4>
+                    <p>Calle América esq. San Alberto</p>
+                </div>
             </div>
-        </div>-->
+            <h2 class="titulo-final">&copy; S. Kimberly Marquina Ch. | UATF Potosí </h2>
+        </footer>
     </div>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
 
 @section('saludo')
 <div class="caratula-ind col-md-6">
-    <h1 style="color: #fff;"> <b> "Sport La Comita" </b></h1>
-    <h2 class="lead text-justify" style="color: #ffd700;"><strong> Bienvenidos a nuestra página web, registrate para poder realizar pedidos y cotizaciones en linea y te contactaremos para coordinar la entrega, puedes realizar tus pedidos cuando estés seguro.</strong>
+    <h1 class="text-white"> <b> "Sport La Comita" </b></h1>
+    <span class="lead text-justify text-warning d-none d-sm-none d-md-block"  ><strong> Bienvenidos a nuestra página web, registrate para poder realizar pedidos y cotizaciones en linea y te contactaremos para coordinar la entrega, puedes realizar tus pedidos cuando estés seguro.</strong>
+    </span>
     </h2>
 
     @if (Route::has('login'))
@@ -13,10 +14,10 @@
             @auth
                 <a href="{{ url('/admin') }}"  class="btn btn-lg  btn-outline-light" >INICIO</a>
             @else
-                <a href="{{ route('login') }}" class="btn btn-lg  btn-light">INGRESAR</a>
+                <a href="{{ route('login') }}" class="btn btn-blog btn-light">INGRESAR</a>
 
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn btn-lg  btn-outline-light">REGISTRARSE</a>
+                    <a href="{{ route('register') }}" class="btn btn-blog  btn-outline-light">REGISTRARSE</a>
                 @endif
             @endauth
     @endif
