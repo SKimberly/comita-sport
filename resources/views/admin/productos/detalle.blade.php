0e847846d183daaef43f9b0242bf362975df27c0
SKimberly <kimi.uatf@gmail.com>
2020-06-15 04:22:12 -0400

    Terminado estilos de detalle de producto

--- resources/views/admin/productos/detalle.blade.php
+++ resources/views/admin/productos/detalle.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
+
 @section('titulo','Listar Productos')
+
 @section('cabecera')
 <div class="content-header">
     <div class="container-fluid">
@@ -18,107 +20,183 @@
     </div><!-- /.container-fluid -->
 </div>
 @endsection
+
 @section('contenido')
 <section class="content">
-    <div class="container-fluid">
+  <div class="container-fluid">
       <div class="col-12 col-sm-10 col-lg-10 mx-auto">
-          <div class="card shadow">
+        <div class="card shadow" >
             <div class="card-body">
               <div class="row">
-                <div class="col-12 col-sm-6 ">
-                    <div class="row">
-                      <div class="photo-container-detalle my-5">
+                  <div class="col-md-5">
+                      <div class="row photo-container-detalle my-3 shadow">
                           <div class="photo-main-detalle">
-                          <div class="controls-detalle">
-                            <i class="fas fa-share-alt" ></i>
-                            <i class="fas fa-heart"></i>
-                          </div>
-                          <div class="col-12 product-thumb">
-                            <img id="featured" src="{{ asset($producto->detalleimagenurl) }}" alt="toaster"/>
-                          </div>
-                          </div>
-                          <div class="col-12">
-                              <div class="row justify-content-center pt-3">
-                                  <ul class="product-image--list">
-                                      @foreach($producto->fotos as $img)
-                                        <li class="item-selected">
-                                            <img src="{{ asset($img->imagen) }}" class="product-image--item"/>
-                                        </li>
-                                      @endforeach
-                                  </ul>
+                              @if($producto->descuento)
+                              <div class="ribbon ribbon-top-right">
+                                  <span><strong>{{ $producto->descuento }}% DES</strong></span>
+                              </div>
+                              @endif
+                              <div class="controls-detalle">
+                                <i class="fas fa-share-alt" ></i>
+                                <i class="fas fa-heart"></i>
+                              </div>
+                              <div class="product-thumb">
+                                <img id="featured" src="{{ asset($producto->detalleimagenurl) }}" alt="toaster"/>
                               </div>
                           </div>
+                          <div class="justify-content-center btn-comita pt-3 pl-3 pr-3" style="border-radius: 5px;">
+                              <ul class="product-image--list">
+                                  @foreach($producto->fotos as $img)
+                                    <li class="item-selected">
+                                        <img src="{{ asset($img->imagen) }}" class="product-image--item"/>
+                                    </li>
+                                  @endforeach
+                              </ul>
+                          </div>
                       </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6">
-                    <div class="title_prodetalle">
-                      <h1>{{ $producto->nombre }}</h1>
-                      <span>{{ $producto->codigo }}</span>
-                    </div>
-                    <div class="bg-light  py-2 px-3 mt-4 border-0">
-                      <h2 class="mb-0">
-                        Bs. {{ $producto->precio }}
-                      </h2>
-                      <h4 class="mt-0">
-                        <div class="description_prodetalles">
-                          <h3><small>
-                          Apartir de {{ $producto->cant_descuento }} Unid. Descuento del {{ $producto->descuento}} %. </small></h3>
-                      </h4>
-                    </div>
-                    <div class="description_prodetalles">
-                        <h3>TALLAS</h3>
-                        <div class="btn-group btn-group-toggle bg-light " data-toggle="buttons">
-                          @foreach($producto->tallas as $talla)
-                          <label class="btn btn-default text-center">
-                            <input type="radio" name="color_option"    >
-                            <span class="text-sm">{{ $talla->nombre }}</span>
-                          </label>
-                          @endforeach
-                        </div>
-                    </div>
-                    <div class="description_prodetalles">
-                        <h3>CATEGORÍA</h3>
-                        <div class="btn-group btn-group-toggle bg-light " data-toggle="buttons">
-                          <label class="btn btn-default text-center">
-                            {{ $categoria->nombre }}
-                          </label>
-                        </div>
-                    </div>
-                    <div class="description_prodetalles">
-                        <h3>DESCRIPCION</h3>
-                        <p class="bg-light  text-justify">
-                            {{ $producto->descripcion }}
-                        </p>
-                    </div>
-                    <div class="description_prodetalles">
-                        <h3>OFERTA</h3>
-                        <p class="bg-light  text-justify">
-                            {{ $producto->oferta }}
-                        </p>
-                    </div>
-                    <div class="description_prodetalles">
-                        <h3>CANTIDAD</h3>
-                        <input type="number" class="form-control col-md-6" min="1" value="1">
-                    </div>
-                    <div class="form-group row mb-0">
-                      <div class="col-md-6 offset-md-2">
-                        <button class="btn btn-comita btn-lg   text-white">
-                          <i class="fas fa-cart-plus fa-lg mr-2"></i>
-                          AÑADIR AL CARRITO
-                        </button>
+                  </div>
+                  <div class="col-md-7">
+                      <div class="cynta_estado">
+                          <span> <strong> DISPONIBLE </strong> </span>
                       </div>
-                    </div>
-                </div>
+                      <div class="title_prodetalle">
+                        <h1>{{ $producto->nombre }}</h1>
+                        <span class="text-secondary">{{ $producto->codigo }}</span>
+                      </div>
+                      <div class="cynta_estado">
+                          @if($producto->estado)
+                            <span style="color:cyan;"> <strong> DISPONIBLE </strong> </span>
+                          @else
+                            <span style="color:red;"> <strong> AGOTADO </strong> </span>
+                          @endif
+                      </div>
+                      <hr class="mt-0 mb-0">
+                      <div class="precio_prodetalle  ">
+                          <h2>
+                            Bs. {{ $producto->precio }}
+                          </h2>
+                          <span class="text-muted">
+                              Apartir de {{ $producto->cant_descuento }} Unid. Descuento del {{ $producto->descuento}} %.
+                          </span>
+                      </div>
+                      <hr class="mt-0 mb-0">
+                      <div class="subti_prodetalle">
+                          <div class="form-group row">
+                            <div class="col-sm-7">
+                                <h3>TALLAS</h3>
+                                <div class="product-talla">
+                                    <label class="checkbox-btn">
+                                      <input type="checkbox">
+                                        <span class="btn btn-light-checkbox"> S </span>
+                                    </label>
+                                    <label class="checkbox-btn">
+                                      <input type="checkbox">
+                                        <span class="btn btn-light-checkbox"> M </span>
+                                    </label>
+                                    <label class="checkbox-btn">
+                                      <input type="checkbox">
+                                      <span class="btn btn-light-checkbox"> XXL </span>
+                                    </label>
+                                    <label class="checkbox-btn">
+                                        <input type="checkbox">
+                                          <span class="btn btn-light-checkbox"> XXXL </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-sm-5">
+                                <h3>CATEGORÍA</h3>
+                                <div class="product-talla">
+                                    <label class="checkbox-btn">
+                                        <span class="btn btn-light-checkbox" style="background-color: cyan;"> {{ $categoria->nombre }} </span>
+                                    </label>
+                                </div>
+                            </div>
+                          </div>
+                      </div>
+                      <hr class="mt-0 mb-0">
+                      <div class="subti_prodetalle  ">
+                            <h3>DESCRIPCION</h3>
+                            <p class="text-justify">
+                                {{ $producto->descripcion }}
+                            </p>
+                      </div>
+                      <hr class="mt-0 mb-0">
+                      <div class="subti_prodetalle  ">
+                          <h3>OFERTA</h3>
+                          <p class="text-justify">
+                              {{ $producto->oferta }}
+                          </p>
+                      </div>
+                      <hr class="mt-0 mb-0">
+                      <div class="subti_prodetalle">
+                          <div class="form-group row">
+                            <div class="col-sm-7">
+                                <h3>CANTIDAD</h3>
+                                <div class="number-input">
+                                  <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" > <i class="fas fa-minus" style="color:#0a2b4e;"></i>
+                                  </button>
+                                  <input class="quantity" min="1" name="quantity" value="1" type="number">
+                                  <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"> <i class="fas fa-plus" style="color:#0a2b4e;"></i>
+                                  </button>
+                                </div>
+                            </div>
+                            <div class="col-sm-5">
+                                <h3>STOCK</h3>
+                                <div class="product-talla">
+                                    <label class="checkbox-btn" >
+                                      <span class="btn btn-light-checkbox" style="background-color: cyan;"> {{ $producto->stock }} Unid.</span>
+                                    </label>
+                                </div>
+                            </div>
+                          </div>
+                      </div>
+                      <hr class="mt-0 mb-2">
+                      <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-3">
+                          <button class="btn btn-comita text-white btn-lg btn_deta_pro ">
+                              <span style="color:cyan">
+                                  <i class="fas fa-cart-plus fa-lg mr-2"></i>
+                                AÑADIR AL CARRITO
+                              </span>
+                          </button>
+                        </div>
+                      </div>
+                  </div>
               </div>
             </div>
-          </div>
+        </div>
       </div>
   </div>
 </section>
 @endsection
+
+@push('styles')
+<style>
+.btn-light-checkbox {
+  font-size: 15px !important;
+  border-radius: 25px !important;
+  /*-webkit-border-radius: 25px !important;*/
+}
+
+.btn_deta_pro {
+  border-radius: 25px !important;
+}
+
+.number-input.number-input {
+    border: 1px solid cyan;
+    background-color: #fff;
+    border-radius: .25rem;
+}
+
+.number-input.number-input input[type="number"] {
+    max-width: 5rem;
+}
+
+</style>
+@endpush
+
 @push('scripts')
-<script>
+<script type="text/javascript">
   $('.product-image--list li').hover(function() {
       var url = $(this).children('img').attr('src');
       $('.item-selected').removeClass('item-selected');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,62 +25,75 @@
     <div class="row">
         @foreach($productos as $producto)
         <div class="col-md-4">
-            <div class="product-card mb-30">
+            <div class="card-home mb-30 shadow">
                 @if($producto->descuento)
                 <div class="ribbon ribbon-top-right">
-                    <span>{{ $producto->descuento }}% DES</span>
+                    <span><strong> {{ $producto->descuento }}% DES </strong></span>
                 </div>
                 @endif
-                <div class="bbb_deals_title">{{ $producto->nombre }}</div>
-                <div class="product-badge bg-nuevo border-default text-body"> <strong> {{ $producto->precio }} Bs. </strong></div>
+
+                <div class="car_home_precio btn-comita">
+                    <strong> {{ $producto->precio }} Bs. </strong>
+                </div>
 
                   <div class="product-thumb">
-                    <img id="featured" src="{{ asset($producto->detalleimagenurl) }}" alt="toaster"/>
+                    <img id="featured" src="{{ asset($producto->detalleimagenurl) }}" alt="toaster" />
                   </div>
 
-                  <ul class=" row justify-content-center product-image--list">
+                  <!--<ul class=" row justify-content-center product-image--list">
                     @foreach($producto->fotos as $img)
                       <li class="item-selected">
                           <img src="{{ asset($img->imagen) }}" class="product-image--item"/>
                       </li>
                     @endforeach
-                  </ul>
+                  </ul>-->
 
-                <div class="product-">
-                    <!--<div class="product-category"><a href="#" data-abc="true">{{ $producto->nombre }}</a></div>-->
-                    <h3 class="product-title text-justify"><a href="#" data-abc="true" > {{ $producto->descripcion }}</a></h3>
+                <div class="product_card_home">
+                    <div class="product-nombre">
+                      <a href="#" data-abc="true"><strong> {{ $producto->nombre }} </strong></a>
+                    </div>
+                    <div class="product-description text-justify">
+                        <a href="#" data-abc="true" > {{ $producto->descripcion }}</a>
+                    </div>
 
-                    <div class="tallas-css"><strong>Tallas:</strong>
-                            <label class="checkbox-btn">
-                              <input type="checkbox">
-                                <span class="btn btn-light-checkbox"> XS </span>
-                            </label>
-                            <label class="checkbox-btn">
-                              <input type="checkbox">
-                                <span class="btn btn-light-checkbox"> SM </span>
-                            </label>
-                            <label class="checkbox-btn">
-                              <input type="checkbox">
-                              <span class="btn btn-light-checkbox"> XXL </span>
-                            </label>
-                            <label class="checkbox-btn">
-                                <input type="checkbox">
-                                  <span class="btn btn-light-checkbox"> XXXL </span>
-                            </label>
+                    <div class="product-talla">
+                        <strong>Tallas:</strong>
+                        <label class="checkbox-btn">
+                          <input type="checkbox">
+                            <span class="btn btn-light-checkbox"> XS </span>
+                        </label>
+                        <label class="checkbox-btn">
+                          <input type="checkbox">
+                            <span class="btn btn-light-checkbox"> SM </span>
+                        </label>
+                        <label class="checkbox-btn">
+                          <input type="checkbox">
+                          <span class="btn btn-light-checkbox"> XXL </span>
+                        </label>
+                        <label class="checkbox-btn">
+                            <input type="checkbox">
+                              <span class="btn btn-light-checkbox"> XXXL </span>
+                        </label>
                     </div>
                 </div>
                 <div class="product-button-group">
-                    <span class="product-button btn-compare" data-abc="true">
-                      <input min="1" value="1" class="form-control" type="number">
-                      <span >Cantidad</span>
+                    <span class="product-button btn-wishlist input-group-sm" data-abc="true">
+                      <!--<input type="number" class="form-control border-0" min="1" value="1" style="background-color: cyan;">-->
+                      <div class="number-input">
+                          <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" > <i class="fas fa-minus" style="color:#0a2b4e;"></i>
+                          </button>
+                          <input class="form-control" min="1" name="quantity" value="1" type="number">
+                          <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"> <i class="fas fa-plus" style="color:#0a2b4e;"></i>
+                          </button>
+                      </div>
+                      <!--<span style="color:cyan;" >Cantidad</span>-->
                     </span>
-                    <a class="product-button btn-wishlist" href="{{ route('admin.producto.detalles', [$producto->slug]) }}" data-abc="true">
-                            <i class="fas fa-eye" style="color:#FFC300;"></i>
-                        </button>
+                    <a class="product-button btn-compare" href="{{ route('admin.producto.detalles', [$producto->slug]) }}" target="__blanck" data-abc="true">
+                        <i class="fas fa-eye" style="color:cyan;"></i>
                         <span>Ver Detalles</span>
                     </a>
-                    <a class="product-button btn-wishlist" href="#" data-abc="true">
-                      <i class="fas fa-cart-plus" style="color:#FFC300;"></i>
+                    <a class="product-button" href="#" data-abc="true">
+                      <i class="fas fa-cart-plus" style="color:cyan;"></i>
                       <span>Añadir al carrito</span>
                     </a>
                 </div>
@@ -102,25 +115,6 @@
       $('#featured').attr('src', url);
 
   });
-</script>
-<script>
-$('#verDeta').on('show.bs.modal', function (event) {
-
-    var button = $(event.relatedTarget)
-    var pro_id = button.data('id')
-    var pro_nom = button.data('nom')
-    var pro_des = button.data('des')
-    var pro_stock = button.data('stock')
-    var pro_oferta = button.data('ofer')
-    var modal = $(this)
-    modal.find('.modal-body #proid').val(pro_id);
-    modal.find('.modal-body #pronom').val(pro_nom);
-    modal.find('.modal-body #prodes').val(pro_des);
-    modal.find('.modal-body #prosto').val(pro_stock);
-    modal.find('.modal-body #profer').val(pro_oferta);
-})
-</script>
-<script>
     swal.fire({
       icon: 'success',
       title: 'Gracias por su visita!',
